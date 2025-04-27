be306c4448195c9bdfcc1778ccc85a916d4d9a4a
CleitonCorrea <cleitoncorrea2013@hotmail.com>
2025-04-27 12:48:54 -0300

    configuração do JPA e PostgreSQL

---
+++ src/main/java/com/cleitoncorrea/desafio_nubanck/model/Clientes.java
@@ -0,0 +1,30 @@
+package com.cleitoncorrea.desafio_nubanck.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Clientes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @OneToMany(mappedBy = "clientes", cascade = CascadeType.ALL, orphanRemoval = true )
+    @JsonManagedReference
+    private List<Contatos> contatos = new ArrayList<Contatos>();
+
+}
